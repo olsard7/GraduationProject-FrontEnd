8fdbd2c1bcff475b5bd432223ef83627453741a3
frontendoleg <frontendoleg@gmail.com>
2016-05-19 10:19:07 +0300

    my project

---
+++ js/HtmlMore.js
@@ -0,0 +1,36 @@
+function LoadBruschetta()
+{
+	$('#Bruschetta').load("HtmlShowMore/Bruschetta.html");
+}
+function LoadCalzone()
+{
+	$('#Calzone').load("HtmlShowMore/Calzone.html");
+}
+function LoadCannelloni()
+{
+	$('#Cannelloni').load("HtmlShowMore/Cannelloni.html");
+}
+function LoadCannoli()
+{
+	$('#Cannoli').load("HtmlShowMore/Cannoli.html");
+}
+function LoadCaprese()
+{
+	$('#Caprese').load("HtmlShowMore/Caprese.html");
+}
+function LoadFrittata()
+{
+	$('#Frittata').load("HtmlShowMore/Frittata.html");
+}
+function LoadGnocchi()
+{
+	$('#Gnocchi').load("HtmlShowMore/Gnocchi.html");
+}
+function LoadMilanese()
+{
+	$('#Milanese').load("HtmlShowMore/Milanese.html");
+}
+function LoadTiramisu()
+{
+	$('#Tiramisu').load("HtmlShowMore/Tiramisu.html");
+}
